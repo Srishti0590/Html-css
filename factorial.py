2825349f2fa8759fff1ff47709c298feed8722b3
Srishti0590 <64901992+Srishti0590@users.noreply.github.com>
2021-09-14 14:43:34 +0545

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,12 @@
+import time
+start_time = time.time()
+def fact(n):
+    product = 1
+    for i in range(n):
+        product = product * (i+1)
+    return product
+print(fact(5))
+end_time = time.time()
+print(end_time-start_time)
+
+
